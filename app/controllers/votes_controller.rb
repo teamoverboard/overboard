de5f53de74542dd77b5d2dfae812b2e029ecb362
npostolovski <npostolovski@gmail.com>
2014-10-18 15:37:34 +1100

    Users cannot vote multiple times for the same question or answer

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,8 @@
 class VotesController < ApplicationController
 
   def upvote
-    @vote = Vote.new(voteable_id:   params[:voteable_id], 
-    	               voteable_type: params[:voteable_type],
-    	               user_id:       params[:user_id])
-
-    redirect_to(:back) if @vote.save
+    @vote = Vote.create(voteable_id: params[:voteable_id], 
+    	voteable_type: params[:voteable_type], user_id: params[:user_id])
+    redirect_to(:back)
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,4 @@
 class Vote < ActiveRecord::Base
 	belongs_to :voteable, polymorphic: true, counter_cache: true
+	validates :user_id, uniqueness: { scope: [ :voteable_id, :voteable_type ] }
 end
