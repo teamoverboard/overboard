62a811506e5577994ce278be433af58c18d2d356
npostolovski <npostolovski@gmail.com>
2014-10-18 15:29:42 +1100

    Users cannot vote if not logged in

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,8 @@ class VotesController < ApplicationController
 
   def upvote
     @vote = Vote.new(voteable_id:   params[:voteable_id], 
-    	               voteable_type: params[:voteable_type])
+    	               voteable_type: params[:voteable_type],
+    	               user_id:       params[:user_id])
 
     redirect_to(:back) if @vote.save
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018034355) do
+ActiveRecord::Schema.define(version: 20141018042143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20141018034355) do
     t.string   "voteable_type"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
   end
 
 end
