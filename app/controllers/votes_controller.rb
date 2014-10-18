d270ed0bd74d722c482e64527751411fe43997b0
npostolovski <npostolovski@gmail.com>
2014-10-18 14:44:13 +1100

    People can upvote answers

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,8 +3,7 @@ class VotesController < ApplicationController
   def upvote
     @vote = Vote.new(voteable_id:   params[:voteable_id], 
     	               voteable_type: params[:voteable_type])
-    
-    redirect_to root_path if @vote.save
-  end
 
+    redirect_to(:back) if @vote.save
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018021808) do
+ActiveRecord::Schema.define(version: 20141018033615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141018021808) do
     t.text     "content"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "votes_count"
   end
 
   create_table "questions", force: true do |t|
