e5eb88b8d1941dbed27f89a74844304e26619a37
Martin Jagusch <_@mj.io>
2014-10-18 16:45:29 +1100

    Convert Markdown text to html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { "#{page_title} • Overboard" }
   end
+
+  def parse(text)
+    HTML::Pipeline::MarkdownFilter.new(text).call
+  end
 end
