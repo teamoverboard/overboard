54e168576d6f0d15d1839366e5e35b28b69339b7
Martin Jagusch <_@mj.io>
2014-10-19 13:17:59 +1100

    Add PgwModal dependency

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
-//= require rainbow
-
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require rainbow
+//= require pgwmodal
 //= require autocomplete-rails
 //= require_tree .
 
